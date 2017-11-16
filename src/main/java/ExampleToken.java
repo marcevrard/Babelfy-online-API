da51d7ae08f4915da4be765332d0e23c1c9b5f8e
Marc Evrard <marc.evrard@gmail.com>
2017-11-16 13:54:23 +0900

    Cleanup

--- src/main/java/ExampleToken.java
+++ src/main/java/ExampleToken.java
@@ -1,4 +1,7 @@
 import java.util.List;
+import java.io.File;
+import java.util.Scanner;
+import java.io.PrintWriter;
 import it.uniroma1.lcl.babelfy.core.Babelfy;
 import it.uniroma1.lcl.babelfy.commons.BabelfyConstraints;
 import it.uniroma1.lcl.babelfy.commons.BabelfyParameters;
@@ -10,9 +13,13 @@ import it.uniroma1.lcl.babelfy.commons.annotation.SemanticAnnotation.Source;
 import it.uniroma1.lcl.babelfy.commons.annotation.TokenOffsetFragment;
 import it.uniroma1.lcl.jlt.util.Language;
 
-public class Example {
+public class ExampleToken {
 	public static void main(String[] args) throws Exception {
-		String inputText = "BabelNet is both a multilingual encyclopedic dictionary and a semantic network";
+		File file = new File("input/input.txt");
+		Scanner scanner = new Scanner(file);
+		String inputText = scanner.useDelimiter("\\Z").next();
+		scanner.close();
+
 		BabelfyConstraints constraints = new BabelfyConstraints();
 		SemanticAnnotation a = new SemanticAnnotation(new TokenOffsetFragment(0, 0), "bn:03083790n",
 				"http://dbpedia.org/resource/BabelNet", Source.OTHER);
@@ -26,15 +33,24 @@ public class Example {
 		Babelfy bfy = new Babelfy(bp);
 		List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(inputText, Language.EN, constraints);
 
+		// Create printer
+		PrintWriter writer = new PrintWriter("output/output.txt", "UTF-8");
+
 		// bfyAnnotations is the result of Babelfy.babelfy() call
 		for(SemanticAnnotation annotation:bfyAnnotations) {
 			// splitting the input text using the CharOffsetFragment start and end anchors
-			String frag = inputText.substring(annotation.getCharOffsetFragment().getStart(),
-					annotation.getCharOffsetFragment().getEnd() + 1);
-			System.out.println(frag + "\t" + annotation.getBabelSynsetID());
-			System.out.println("\t" + annotation.getBabelNetURL());
-			System.out.println("\t" + annotation.getDBpediaURL());
-			System.out.println("\t" + annotation.getSource());
+			Integer offsetStart = annotation.getCharOffsetFragment().getStart();
+			Integer offsetEnd = annotation.getCharOffsetFragment().getEnd();
+			String frag = inputText.substring(offsetStart, offsetEnd + 1);
+			String output = annotation.getBabelSynsetID() + "\t"
+							+ offsetStart.toString() + "\t"
+							+ offsetEnd.toString() + "\t"
+							+ annotation.getSource() + "\t"
+							+ frag;
+			System.out.println(output);
+			// Print to file
+			writer.println(output);
 		}
+		writer.close();
 	}
 }
