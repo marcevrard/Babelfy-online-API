58b6cf704038bdfe451f21570c8965a6d6100e6f
Marc Evrard <marc.evrard@gmail.com>
2017-11-17 17:54:04 +0900

    Conv indentation to space

--- src/main/java/ExampleFile.java
+++ src/main/java/ExampleFile.java
@@ -15,51 +15,51 @@ import it.uniroma1.lcl.jlt.util.Language;
 
 public class ExampleFile {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		// Get input text from file
-		Path currentPath = Paths.get(System.getProperty("user.dir"));
-		String fileName = "se2013_doc1.txt";	// toy_example.txt
-		Path file = Paths.get(currentPath.toString(), "input", fileName);
-		Scanner scanner = new Scanner(file);
-		scanner.useDelimiter("\\Z");  // \Z = end of the string anchor
-		String inputText = scanner.next();
-		scanner.close();
+        // Get input text from file
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        String fileName = "se2013_doc1.txt";	// toy_example.txt
+        Path file = Paths.get(currentPath.toString(), "input", fileName);
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter("\\Z");  // \Z = end of the string anchor
+        String inputText = scanner.next();
+        scanner.close();
 
-		// Setup Babelfy paramteres
-		BabelfyParameters bfyParams = new BabelfyParameters();
-		bfyParams.setAnnotationResource(SemanticAnnotationResource.BN);
-		bfyParams.setMCS(MCS.ON_WITH_STOPWORDS);
-		bfyParams.setScoredCandidates(ScoredCandidates.TOP);
+        // Setup Babelfy paramteres
+        BabelfyParameters bfyParams = new BabelfyParameters();
+        bfyParams.setAnnotationResource(SemanticAnnotationResource.BN);
+        bfyParams.setMCS(MCS.ON_WITH_STOPWORDS);
+        bfyParams.setScoredCandidates(ScoredCandidates.TOP);
 
-		// Launch Babelfy annotation
-		Babelfy bfy = new Babelfy(bfyParams);
-		List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(inputText, Language.EN);
+        // Launch Babelfy annotation
+        Babelfy bfy = new Babelfy(bfyParams);
+        List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(inputText, Language.EN);
 
-		// Create printer
-		Path out_dir = Paths.get("output");
-		Files.createDirectories(out_dir);
-		String out_fileName = "out_" + fileName + ".tsv";
-		Path out_file = Paths.get(currentPath.toString(), out_dir.toString(), out_fileName);
-		PrintWriter writer = new PrintWriter(out_file.toString(), "UTF-8");
+        // Create printer
+        Path out_dir = Paths.get("output");
+        Files.createDirectories(out_dir);
+        String out_fileName = "out_" + fileName + ".tsv";
+        Path out_file = Paths.get(currentPath.toString(), out_dir.toString(), out_fileName);
+        PrintWriter writer = new PrintWriter(out_file.toString(), "UTF-8");
 
-		// bfyAnnotations is the result of Babelfy.babelfy() call
-		for(SemanticAnnotation annotation:bfyAnnotations) {
-			// splitting the input text using the CharOffsetFragment start and end anchors
-			Integer offsetStart = annotation.getCharOffsetFragment().getStart();
-			Integer offsetEnd = annotation.getCharOffsetFragment().getEnd();
-			String frag = inputText.substring(offsetStart, offsetEnd + 1);
-			String output = annotation.getBabelSynsetID() + "\t"
-							+ annotation.getTokenOffsetFragment().getStart() + "\t"
-							+ annotation.getTokenOffsetFragment().getEnd() + "\t"
-							+ offsetStart.toString() + "\t"
-							+ offsetEnd.toString() + "\t"
-							+ annotation.getSource() + "\t"
-							+ frag;
-			// System.out.println(output);
-			// Print to file
-			writer.println(output);
-		}
-		writer.close();
-	}
+        // bfyAnnotations is the result of Babelfy.babelfy() call
+        for(SemanticAnnotation annotation:bfyAnnotations) {
+            // splitting the input text using the CharOffsetFragment start and end anchors
+            Integer offsetStart = annotation.getCharOffsetFragment().getStart();
+            Integer offsetEnd = annotation.getCharOffsetFragment().getEnd();
+            String frag = inputText.substring(offsetStart, offsetEnd + 1);
+            String output = annotation.getBabelSynsetID() + "\t"
+                            + annotation.getTokenOffsetFragment().getStart() + "\t"
+                            + annotation.getTokenOffsetFragment().getEnd() + "\t"
+                            + offsetStart.toString() + "\t"
+                            + offsetEnd.toString() + "\t"
+                            + annotation.getSource() + "\t"
+                            + frag;
+            // System.out.println(output);
+            // Print to file
+            writer.println(output);
+        }
+        writer.close();
+    }
 }
--- src/main/java/ExampleToken.java
+++ src/main/java/ExampleToken.java
@@ -20,65 +20,65 @@ import it.uniroma1.lcl.jlt.util.Language;
 
 public class ExampleToken {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		// Get input data from file
-		Path currentPath = Paths.get(System.getProperty("user.dir"));
-		String fileName = "se2013_tokens_1.tsv";
-		Path file = Paths.get(currentPath.toString(), "input/se2013_tokens", fileName);
-		Scanner scanner = new Scanner(file);
+        // Get input data from file
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        String fileName = "se2013_tokens_1.tsv";
+        Path file = Paths.get(currentPath.toString(), "input/se2013_tokens", fileName);
+        Scanner scanner = new Scanner(file);
 
-		List<BabelfyToken> tokenizedInput = new ArrayList<>();
+        List<BabelfyToken> tokenizedInput = new ArrayList<>();
 
-		// Convert POS input to POS objects
-		Map<String, PosTag> posDic = new HashMap<String, PosTag>();
-		posDic.put("n", PosTag.NOUN);
-		posDic.put("v", PosTag.VERB);
-		posDic.put("j", PosTag.ADJECTIVE);
-		posDic.put("r", PosTag.ADVERB);
-		posDic.put("x", PosTag.OTHER);
+        // Convert POS input to POS objects
+        Map<String, PosTag> posDic = new HashMap<String, PosTag>();
+        posDic.put("n", PosTag.NOUN);
+        posDic.put("v", PosTag.VERB);
+        posDic.put("j", PosTag.ADJECTIVE);
+        posDic.put("r", PosTag.ADVERB);
+        posDic.put("x", PosTag.OTHER);
 
-		// Input tokens to Babelfy
-		while (scanner.hasNext()) {
-			String line = scanner.nextLine();
-			String[] lineParts = line.split("\\t");
-			if (lineParts[0].equals("<eos>")) {
-				tokenizedInput.add(BabelfyToken.EOS);
-			} else {
-				tokenizedInput.add(new BabelfyToken(lineParts[0], posDic.get(lineParts[1])));
-			}
-		}
-		scanner.close();
+        // Input tokens to Babelfy
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] lineParts = line.split("\\t");
+            if (lineParts[0].equals("<eos>")) {
+                tokenizedInput.add(BabelfyToken.EOS);
+            } else {
+                tokenizedInput.add(new BabelfyToken(lineParts[0], posDic.get(lineParts[1])));
+            }
+        }
+        scanner.close();
 
-		// Setup Babelfy paramteres
-		BabelfyParameters bfyParams = new BabelfyParameters();
-		bfyParams.setAnnotationResource(SemanticAnnotationResource.BN);
-		bfyParams.setMCS(MCS.ON_WITH_STOPWORDS);
-		bfyParams.setScoredCandidates(ScoredCandidates.TOP);
+        // Setup Babelfy paramteres
+        BabelfyParameters bfyParams = new BabelfyParameters();
+        bfyParams.setAnnotationResource(SemanticAnnotationResource.BN);
+        bfyParams.setMCS(MCS.ON_WITH_STOPWORDS);
+        bfyParams.setScoredCandidates(ScoredCandidates.TOP);
 
-		// Launch Babelfy annotation
-		Babelfy bfy = new Babelfy(bfyParams);
-		List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(tokenizedInput, Language.EN);
+        // Launch Babelfy annotation
+        Babelfy bfy = new Babelfy(bfyParams);
+        List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(tokenizedInput, Language.EN);
 
-		// Create printer
-		Path out_dir = Paths.get("output");
-		Files.createDirectories(out_dir);
-		String out_fileName = "out_" + fileName;
-		Path out_file = Paths.get(currentPath.toString(), out_dir.toString(), out_fileName);
-		PrintWriter writer = new PrintWriter(out_file.toString(), "UTF-8");
+        // Create printer
+        Path out_dir = Paths.get("output");
+        Files.createDirectories(out_dir);
+        String out_fileName = "out_" + fileName;
+        Path out_file = Paths.get(currentPath.toString(), out_dir.toString(), out_fileName);
+        PrintWriter writer = new PrintWriter(out_file.toString(), "UTF-8");
 
-		// bfyAnnotations is the result of Babelfy.babelfy() call
-		for(SemanticAnnotation annotation:bfyAnnotations) {
-			// splitting the input text using the CharOffsetFragment start and end anchors
-			String output = annotation.getBabelSynsetID() + "\t"
-							+ annotation.getTokenOffsetFragment().getStart() + "\t"
-							+ annotation.getTokenOffsetFragment().getEnd() + "\t"
-							+ annotation.getSource() + "\t"
-							+ annotation.getDBpediaURL();
-			// System.out.println(output);
-			// Print to file
-			writer.println(output);
-		}
-		writer.close();
-	}
+        // bfyAnnotations is the result of Babelfy.babelfy() call
+        for(SemanticAnnotation annotation:bfyAnnotations) {
+            // splitting the input text using the CharOffsetFragment start and end anchors
+            String output = annotation.getBabelSynsetID() + "\t"
+                            + annotation.getTokenOffsetFragment().getStart() + "\t"
+                            + annotation.getTokenOffsetFragment().getEnd() + "\t"
+                            + annotation.getSource() + "\t"
+                            + annotation.getDBpediaURL();
+            // System.out.println(output);
+            // Print to file
+            writer.println(output);
+        }
+        writer.close();
+    }
 }
